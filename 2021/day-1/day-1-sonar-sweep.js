874b7d3a9f118b03db12e9d2c160af2f25e32b0d
caner <canerturkmen@msn.com>
2022-01-01 22:56:51 +0300

    day 1 added

---
+++ 2021/day-1/day-1-sonar-sweep.js
@@ -0,0 +1,36 @@
+fs = require('fs');
+
+// For the local input
+const file = fs
+  .readFileSync('./day-1.1-input.txt')
+  .toString('utf-8')
+  .split('\r\n');
+
+// Part 1
+function countIncreased(file) {
+  let counter = 0;
+
+  for (let i = 0; i < file.length - 1; i++) {
+    if (parseInt(file[i]) < parseInt(file[i + 1])) {
+      counter++;
+    }
+  }
+  console.log(counter);
+}
+
+// Part 2
+function countSlidingIncreased(file) {
+  let counter = 0;
+  for (let i = 0; i < file.length - 2; i++) {
+    if (
+      parseInt(file[i]) + parseInt(file[i + 1]) + parseInt(file[i + 2]) <
+      parseInt(file[i + 1]) + parseInt(file[i + 2]) + parseInt(file[i + 3])
+    ) {
+      counter++;
+    }
+  }
+  console.log(counter);
+}
+
+countIncreased(file);
+countSlidingIncreased(file);
